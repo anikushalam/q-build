cc2280f45c256af149b75a675b8ac8ebe6a2ec8d
anikushalam <rjsingh.ak@gmail.com>
2024-07-30 09:05:24 +0530

    30-07-2024 global

---
+++ build/static/js/94606.8746a106.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmithkal_qviple=self.webpackChunkmithkal_qviple||[]).push([[94606,2160,54035,20420,31743,35067],{63687:(e,l,r)=>{r.r(l),r.d(l,{default:()=>a});var s=r(59393),t=r(80184);const a=e=>{let{customStyle:l,children:r}=e;return(0,t.jsx)("div",{className:s.Z.user_member_container_wrapper,style:l,children:r})}}}]);
+//# sourceMappingURL=94606.8746a106.chunk.js.map
