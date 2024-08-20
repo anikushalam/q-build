42a9d6340845552dadaf17a1fb898067eb0297f6
anikushalam <rjsingh.ak@gmail.com>
2024-08-20 13:15:13 +0530

    20-08-2024 global

---
+++ build/static/js/26215.c980def9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmithkal_qviple=self.webpackChunkmithkal_qviple||[]).push([[26215,81221,58936,16825,30053],{63687:(e,l,r)=>{r.r(l),r.d(l,{default:()=>a});var s=r(59393),t=r(80184);const a=e=>{let{customStyle:l,children:r}=e;return(0,t.jsx)("div",{className:s.Z.user_member_container_wrapper,style:l,children:r})}}}]);
+//# sourceMappingURL=26215.c980def9.chunk.js.map
---
+++ build/static/js/45734.27da176b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmithkal_qviple=self.webpackChunkmithkal_qviple||[]).push([[45734,81221,58936,3253,16825,26215,30053],{63687:(e,l,r)=>{r.r(l),r.d(l,{default:()=>a});var s=r(59393),t=r(80184);const a=e=>{let{customStyle:l,children:r}=e;return(0,t.jsx)("div",{className:s.Z.user_member_container_wrapper,style:l,children:r})}}}]);
+//# sourceMappingURL=45734.27da176b.chunk.js.map
