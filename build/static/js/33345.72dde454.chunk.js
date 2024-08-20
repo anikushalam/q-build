fd22be4f78db17ec4e60856892e3979916be838b
anikushalam <rjsingh.ak@gmail.com>
2024-08-20 14:35:27 +0530

    20-08-2024 global

--- build/static/js/33345.72dde454.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmithkal_qviple=self.webpackChunkmithkal_qviple||[]).push([[33345,7920,54089,29477],{63687:(e,l,r)=>{r.r(l),r.d(l,{default:()=>a});var s=r(59393),t=r(80184);const a=e=>{let{customStyle:l,children:r}=e;return(0,t.jsx)("div",{className:s.Z.user_member_container_wrapper,style:l,children:r})}}}]);
-//# sourceMappingURL=33345.72dde454.chunk.js.map
--- build/static/js/92152.e2110893.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmithkal_qviple=self.webpackChunkmithkal_qviple||[]).push([[92152,77707],{63687:(e,l,r)=>{r.r(l),r.d(l,{default:()=>a});var s=r(59393),t=r(80184);const a=e=>{let{customStyle:l,children:r}=e;return(0,t.jsx)("div",{className:s.Z.user_member_container_wrapper,style:l,children:r})}}}]);
-//# sourceMappingURL=92152.e2110893.chunk.js.map
